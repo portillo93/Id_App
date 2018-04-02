918139db7002741e43fc1abe328f66edb515f2c9
portillo93 <00078415@uca.edu.sv>
2018-04-02 00:06:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/portillo/id_app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.portillo.id_app;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Drawable originalDrawable = getResources().getDrawable(R.drawable.image);
+        Bitmap originalBitmap = ((BitmapDrawable) originalDrawable).getBitmap();
+        RoundedBitmapDrawable roundedDrawable = RoundedBitmapDrawableFactory.create(getResources(), originalBitmap);
+        roundedDrawable.setCornerRadius(originalBitmap.getHeight());
+
+        ImageView imageView = (ImageView) findViewById(R.id.foto);
+
+        imageView.setImageDrawable(roundedDrawable);
+    }
+}
